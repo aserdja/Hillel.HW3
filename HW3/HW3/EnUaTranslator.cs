39183e3e64808ae0ceb202efb735abd8bb32b418
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-06 17:05:43 +0300

    Initial commit

---
+++ HW3/HW3/EnUaTranslator.cs
@@ -0,0 +1,11 @@
+﻿public class EnUaTranslator : Translator
+{
+	public override string TextLanguage => throw new NotImplementedException();
+
+	public override string TargetLanguage => throw new NotImplementedException();
+
+	public override string Translate(string incomingText)
+	{
+		throw new NotImplementedException();
+	}
+}
