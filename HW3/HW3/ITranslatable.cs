71873aa94f2da9b9d1aedb284720ee3cbd4cfba4
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-06 16:41:33 +0300

    Created abstract Translator-class and interface

---
+++ HW3/HW3/ITranslatable.cs
@@ -0,0 +1,4 @@
+﻿public interface ITranslatable
+{
+	public string Translate(string incomingText);
+}
---
+++ HW3/HW3/Translator.cs
@@ -0,0 +1,6 @@
+﻿public abstract class Translator : ITranslatable
+{
+	public abstract string TextLanguage { get; }
+	public abstract string TargetLanguage { get; }
+	public abstract string Translate(string incomingText);
+}
