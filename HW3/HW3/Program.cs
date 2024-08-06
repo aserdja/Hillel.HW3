d3f140b25cfb15c93eee8939af74cbd4bdbfe620
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-06 18:21:16 +0300

    Added data for test

--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using HW3;
+
+var enTranslator = new EnUaTranslator();
+var uaTranslator = new UaEnTranslator();
+
+Console.WriteLine(enTranslator.Translate("Bye-bye"));
+Console.WriteLine(enTranslator.Translate("Hello"));
+Console.WriteLine(enTranslator.Translate("Bye"));
+
+Console.WriteLine();
+
+Console.WriteLine(uaTranslator.Translate("Привіт"));
+Console.WriteLine(uaTranslator.Translate("Як справи"));
+Console.WriteLine(uaTranslator.Translate("Прощавай"));
